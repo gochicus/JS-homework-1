b1d268db5ea401561b5bd48028bde6ce68425d74
gochicus <gochicus@gmail.com>
2019-06-27 21:32:37 +0900

    Homework 6

---
+++ homework6.js
@@ -0,0 +1,21 @@
+
+var $BuyButton = document.getElementsByClassName("buy");
+var $request = document.getElementsByClassName("furniture");
+$BuyButton.addEventListener("click", handlerButtonBuy);
+var cart = 0;
+function handlerButtonBuy($request) {
+    for(var i = 0; i < $request.length ; i++) {
+        var requestValue = $request[i].innerHTML;
+        var catalog = {
+            Chair: {price: 5000},
+            Table: {price: 6000},
+            Shelf: {price: 10000},
+        };
+        for (requestValue in catalog) {
+            cart += requestValue.price;
+        }
+    }
+    var $output = document.createElement("div");
+    $output.innerHTML = "Стоимость корзины:" + cart;
+}
+//
